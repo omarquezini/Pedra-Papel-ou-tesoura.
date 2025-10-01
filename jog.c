29deeaeb942b7f3b772a685f68408438a1b10886
Rafael Marquezini Silva <rafael.marquezini.silva@escola.pr.gov.br>
2025-10-01 21:45:35 +0000

    tentando.

---
+++ jog.c
@@ -0,0 +1,34 @@
+# include <stdio.h>
+
+void poize(){
+    
+        
+}
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+ 
+printf("[]==========================[]\n");
+printf("[]                          []\n");
+printf("[]       PEDRA, PAPEL       []\n");
+printf("[]        OU TESOURA        []\n");
+printf("[]                          []\n");
+printf("[]                          []\n");
+printf("[]==========================[]\n");
+
+    int pptAAA;
+
+    printf("\nPEDRA (1), PAPEL (2) ou TESOURA (3)?");
+    scanf("%d", &pptAAA);
+
+    
+}
